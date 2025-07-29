a15e4ecc3020282b4660f2d601f771ad573f5758
kajal-sharma-20 <kajal0120sharma@gmail.com>
2025-07-29 16:22:30 +0530

    Update page.js

--- my-app/src/app/admindashboard/page.js
+++ my-app/src/app/admindashboard/page.js
@@ -1,5 +1,4 @@
 "use client";
-export const dynamic = "force-dynamic";
 import { useState, useEffect, useRef } from "react";
 import { useRouter } from "next/navigation";
 import { useSession, signOut } from "next-auth/react";
