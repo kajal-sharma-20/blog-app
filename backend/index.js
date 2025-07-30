af07dbb07098efc5801d9378e40bb873526818b6
kajal-sharma-20 <kajal0120sharma@gmail.com>
2025-07-30 15:18:58 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000", // Local frontend
-      "https://blog-app-1-76hf.onrender.com", // Render frontend
+      "https://blog-app-1-2o6j.onrender.com", // Render frontend
     ],
     credentials: true,
   })
