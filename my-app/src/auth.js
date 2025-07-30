c17efb5f7305ab93e12b78caa230f438ef87e1e3
kajal-sharma-20 <kajal0120sharma@gmail.com>
2025-07-30 13:29:18 +0530

    Update auth.js

--- my-app/src/auth.js
+++ my-app/src/auth.js
@@ -12,7 +12,7 @@ export const authOptions = {
       },
 
       async authorize(credentials) {
-        const res = await fetch("https://blog-app-i58g.onrender.com//api/login", {
+        const res = await fetch("https://blog-app-i58g.onrender.com/api/login", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({
